31460f5b4855e6eab54300d8e383a468f7fe2e30
Shricode9096 <116665472+Shricode9096@users.noreply.github.com>
2023-06-22 09:49:15 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+  # import pywhatkit as whatsapp
+        # # Send message and image on WhatsApp using PyWhatKit
+        # whatsapp.sendwhats_image('+918459289424',temp_file.name ,'accident detected at '+ str(c)+" th frame " )
+        # import os
+        # # Delete the temporary file
+        # os.unlink(temp_file.name)
+    
